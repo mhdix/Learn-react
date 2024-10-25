24612b3b7c1455021b14191484f1243419379e4d
mhdix <domanloom@gmail.com>
2024-10-25 06:29:26 -0700

    a

---
+++ src/components/CourseCard.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const CourseCard = ({course}) => {
+  return (
+    <div
+      style={{ border: "1px solid red", padding: "20px", margin: "40px" }}
+    >
+      <img src="" alt="" />
+      <p>{course.title}</p>
+      <div>{course.desc}</div>
+      <p>tag 1</p>
+      <p>
+        {course.tag.map ((i) => {
+            return (
+              <div>
+                <span>{i}</span>
+                <br />
+              </div>
+            );
+        })}
+      </p>
+      <p>{course.start }</p>
+      <button>BUY</button>
+    </div>
+  );
+}
+
+export default CourseCard
