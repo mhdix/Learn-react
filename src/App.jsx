7324713a8c2e2676789255d0f8216242b8827bd6
mhdix <domanloom@gmail.com>
2024-10-26 00:36:34 -0700

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
-import { useState } from 'react'
-import CourseList from './components/CourseList'
-
+const tabData = [
+  {
+    id: "1",
+    title: "Item 1",
+    content: "Lorem, ipsum dolor1.",
+  },
+  {
+    id: "2",
+    title: "Item 2",
+    content: "Lorem, ipsum dolor2.",
+  },
+  {
+    id: "3",
+    title: "Item 3",
+    content: "Lorem, ipsum dolor3.",
+  },
+  {
+    id: "4",
+    title: "Item 4",
+    content: "Lorem, ipsum dolor4.",
+  },
+];
 function App() {
-
   return (
     <>
-      <CourseList />
+      <div className="tab-content">
+        <div className="tab-content__header">
+          <a href="#">Item one</a>
+          <a href="#">Item two</a>
+          <a href="#">Item three</a>
+        </div>
+        <hr />
+        <div className="tab-content__content">
+          <p>content</p>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
